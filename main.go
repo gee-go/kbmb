cd94337ebc56bc3b3d59e9578e63673fd0514876
GeorgeErickson <george.erickson.jr@gmail.com>
2016-04-18 15:30:20 -0400

    Flags

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	"github.com/apex/log"
 	"github.com/apex/log/handlers/cli"
 	"github.com/gee-go/kbmb/crawl"
 )
+
+type Config struct {
+	Host string
+}
+
+func parseFlags() *Config {
+	c := &Config{}
+
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		log.Fatal("Need a file")
+	}
+
+	c.Host = flag.Arg(0)
+	return c
+}
 
 func check(err error) {
 	if err != nil {
@@ -15,8 +33,10 @@ func check(err error) {
 }
 
 func main() {
+	conf := parseFlags()
+
 	log.SetHandler(cli.New(os.Stderr))
-	s, err := crawl.New("gee.io")
+	s, err := crawl.New(conf.Host)
 	check(err)
 
 	check(s.Run())
