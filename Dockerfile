c2b76f6ce6ae52ef833b6999e38f7a7ce1694c63
GeorgeErickson <george.erickson.jr@gmail.com>
2016-04-23 20:18:57 -0400

    Make it work with docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM iron/go:dev
 
+ENV GOPATH /src/go
 WORKDIR /src/go/src/github.com/gee-go/kbmb
 ADD . /src/go/src/github.com/gee-go/kbmb
--- cfg/cfg.go
+++ cfg/cfg.go
@@ -3,6 +3,8 @@ package cfg
 import (
 	"flag"
 	"strings"
+
+	"github.com/k0kubun/pp"
 )
 
 type Cfg struct {
@@ -16,10 +18,12 @@ var (
 
 func FromFlags() *Cfg {
 	c := &Cfg{}
-	flag.StringVar(&c.Redis.URL, "redis-url", "redis://127.0.0.1:6379", "redis url")
+	flag.StringVar(&c.Redis.URL, "redis", "redis://127.0.0.1:6379", "redis url")
 	flag.StringVar(&nsqdHosts, "nsqd", "localhost:4150", "nsqd HTTP address (may be given multiple times)")
 
 	flag.Parse()
 	c.NSQDHosts = strings.Fields(nsqdHosts)
+	pool := c.NewRedisPool()
+	pp.Println(pool.Get().Do("PING"))
 	return c
 }
